e283d6ed3f155111e5b6db635758cdbd9c299eea
LeeZp29 <phanlenguyen290993@gmail.com>
2023-08-14 15:28:18 +0700

    thêm điều hướng và thêm các trang con

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function gotoTop()
 {
     window.scrollTo(0, 0);
 }
+
+function showSubmenu() {
+    var sub = document.getElementById('submenu');
+    console.log(sub.classList);
+    sub.classList.toggle('show');
+}
